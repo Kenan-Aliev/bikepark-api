de037fa9569a9b835bc82438e058bf0f1ffb9c20
Kenan-Aliev <kenan199948@gmail.com>
2021-10-25 16:06:10 +0600

    documented auth routes on swagger

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require("express");
-const server = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 require("dotenv").config();
 const messages = require("./messages/index");
 const authRoutes = require("./routes/auth");
 const bikeRoutes = require("./routes/bike.js");
 const orderRoutes = require("./routes/order");
-const cors = require("cors");
 
 const PORT = process.env.PORT || 8080;
 const dbUrl = process.env.DB_URL;
 
+const options = {
+  definition: {
+    openapi: "3.0.3",
+    info: {
+      title: "BikePark API",
+      version: "1.0.0",
+      description: "Express BikePark API",
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"]
+};
+
+const specs = swaggerJsDoc(options);
+
+const server = express();
+
+server.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 server.use(express.json());
 server.use(cors());
 
--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,149 @@ const messages = require("../messages/index");
 const { registerValidators } = require("../utils/validators");
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      User:
+ *       type: object
+ *       required:
+ *          - email
+ *          - username
+ *          - phone
+ *          - password
+ *       properties:
+ *          _id:
+ *            type: string
+ *            description: The auto-generated id by MongoDB
+ *          email:
+ *            type: string
+ *            unique: true
+ *          username:
+ *            type: string
+ *            unique: true
+ *            min: 3
+ *            max: 15
+ *          phone:
+ *            type: string
+ *            example: "+996553484837"
+ *          role:
+ *            type: string
+ *            default: user
+ *          password: 
+ *            type: string
+ *            min: 6
+ *            max: 56
+ *          orders:
+ *            type: array
+ *            items:
+ *                type: object
+ *                properties:
+ *                   _id:
+ *                      type: string
+ *                      description: The auto-generated id by MongoDB
+ *                   orderNumber:
+ *                      type: number
+ *                      description: The auto generated number by server
+ *                   totalPrice:
+ *                      type: number
+ *                      description: Total price of order
+ *                   madeAt:
+ *                      type: string
+ *                      format: date
+ *                      example: 2019-05-17
+ *                   bikes:
+ *                         type: array
+ *                         items:
+ *                              type: object
+ *                              properties:
+ *                                          _id: 
+ *                                            type: string
+ *                                            description: The auto-generated id by MongoDB
+ *                                          name:
+ *                                            type: string
+ *                                          brand:
+ *                                            type: string
+ *                                          bikeType:
+ *                                            type: string
+ *                                          price:
+ *                                            type: string
+ */
+
+/** 
+ * @swagger
+ * tags:
+ *   name: UserAuthorization
+ *   description: Authorization managing API
+*/
+
+/**
+ * @swagger
+ * /auth/registration:
+ *    post:
+ *     summary: Returns message about activation registration on user email
+ *     tags: [UserAuthorization]
+ *     requestBody:
+ *        required: true
+ *        content:
+ *           application/json:
+ *            schema:
+ *              type: object
+ *              properties : 
+ *                     email :
+ *                        type: string
+ *                     username: 
+ *                        type: string
+ *                     phone: 
+ *                        type: string
+ *                     password: 
+ *                        type: string
+ *              required:
+ *                     - email
+ *                     - username
+ *                     - phone
+ *                     - password
+ *              example: 
+ *                     {
+ *                       email: "kenan1999@gmail.com",
+ *                       username: "kenan",
+ *                       phone: "+996553484837",
+ *                       password: "k2vrrtz"  
+ *                     }
+ *     responses: 
+ *        422:
+ *           description : Some User Schema validation error
+ *           content: 
+ *            application/json:
+ *              schema:
+ *                     type: object
+ *                     properties: 
+ *                        message: 
+ *                          type: string
+ *        500:
+ *           description: Some server error
+ *           content: 
+ *            application/json:
+ *              schema:
+ *                     type: object
+ *                     properties: 
+ *                        message:
+ *                          type: string
+ * 
+ *        200:
+ *           description: Returns a message about confirming registration on user email
+ *           content: 
+ *            application/json:
+ *              schema:
+ *                     type: object
+ *                     properties: 
+ *                        message: 
+ *                          type: string
+ * 
+ *                        
+ *              
+ * 
+ */
 const transporter = nodemailer.createTransport(
   {
     host: "smtp.mail.ru",
@@ -31,18 +174,6 @@ router.post("/registration", registerValidators, async (req, res) => {
       return res.status(422).send({ message: validationErrors.array()[0].msg });
     }
     const { email, username, phone, password } = req.body;
-    const emailExists = await User.findOne({ email });
-    if (emailExists) {
-      return res
-        .status(400)
-        .json({ message: messages.auth.registration.emailExists });
-    }
-    const usernameExists = await User.findOne({ username });
-    if (usernameExists) {
-      return res
-        .status(400)
-        .json({ message: messages.auth.registration.usernameExists });
-    }
     const token = jwt.sign(
       { email, username, phone, password },
       process.env.SECRET_KEY,
@@ -65,6 +196,54 @@ router.post("/registration", registerValidators, async (req, res) => {
     return res.status(500).json({ message: messages.server.error, error });
   }
 });
+
+
+/**
+ * @swagger
+ * /auth/activation:
+ *   post:
+ *     summary: Returns message about user registration on website
+ *     tags: [UserAuthorization]
+ *     requestBody:
+ *        required: true
+ *        content:
+ *           application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                token: 
+ *                  type: string
+ *     responses:
+ *        400:
+ *           description: Returns a message about invalid token
+ *           content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *        500:
+ *           description: Some server error
+ *           content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *        200:
+ *           description: Returns a success message about user registration
+ *           content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ * 
+ *     
+ */
 
 router.post("/activation", async (req, res) => {
   const { token } = req.body;
@@ -113,13 +292,71 @@ router.post("/activation", async (req, res) => {
   });
 });
 
+
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Returns decoded token with user's data
+ *     tags: [UserAuthorization]
+ *     requestBody:
+ *        required: true
+ *        content:
+ *           application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                email: 
+ *                  type: string
+ *                password:
+ *                  type: string
+ *     responses:
+ *        404:
+ *           description: Returns a message if email or password doesn't match
+ *           content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *        500:
+ *           description: Some server error
+ *           content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *        200:
+ *           description: Returns a success login message and user's some data 
+ *           content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                  id:
+ *                    type: string
+ *                  email:
+ *                    type: string
+ *                  token:
+ *                    type: string
+ * 
+ *     
+ */
+
+
 router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const candidate = await User.findOne({ email });
     if (!candidate) {
       return res
-        .status(400)
+        .status(404)
         .json({ message: messages.auth.login.emailNotExists });
     }
     bcrypt.compare(password, candidate.password, (err, result) => {
@@ -129,7 +366,7 @@ router.post("/login", async (req, res) => {
           .json({ message: messages.auth.login.compareError, err });
       } else if (!result) {
         return res
-          .status(400)
+          .status(404)
           .json({ message: messages.auth.login.wrongPassword });
       } else {
         const token = jwt.sign(
